78bba517b827e714f6e42025e3370b95052c6c5e
ashakhova <vasilyevaanna@gmail.com>
2021-03-11 17:12:42 +0300

    build

--- api/app.js
+++ api/app.js
@@ -16,7 +16,7 @@ const apiRouter = require('./routes/api.router');
 const app = express();
 
 app.use(cors());
-
+app.use(express.static('dist'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
