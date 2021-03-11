47773993b29abc284b5ae1969db24f902d0e09df
ashakhova <vasilyevaanna@gmail.com>
2021-03-11 17:48:44 +0300

    fix package.json

--- api/app.js
+++ api/app.js
@@ -43,7 +43,7 @@ app.use('/api/recipes', recipesRouter);
 app.use('/api/spoonacular', apiRouter)
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist'));
+  res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
 
 module.exports = app;
