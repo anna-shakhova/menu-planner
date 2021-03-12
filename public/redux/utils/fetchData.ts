c67cad88b7afece98877e42187f535cece94ef33
ashakhova <vasilyevaanna@gmail.com>
2021-03-12 10:11:31 +0300

    deploying on heroku

--- public/redux/utils/fetchData.ts
+++ public/redux/utils/fetchData.ts
@@ -5,6 +5,10 @@ export const fetchData = async (url: string, method: string, body?: object) => {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(body),
+    crossDomain: true,
+    xhrFields: {
+      withCredentials: true,
+    },
   });
   return response.json();
 };
