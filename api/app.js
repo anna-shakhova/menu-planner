d95533b1959b7c88dadea1b23a1b9e524a352810
ashakhova <vasilyevaanna@gmail.com>
2021-03-12 10:28:26 +0300

    deploying on heroku

--- api/app.js
+++ api/app.js
@@ -35,6 +35,7 @@ app.use(
     store: sessionStore,
     resave: false,
     saveUninitialized: false,
+    proxy : true,
     cookie: {
       secure: process.env.NODE_ENV === 'production',
       maxAge: 1000 * 60 * 60 * 24 * 2,
